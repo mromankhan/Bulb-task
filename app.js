d22d252a09a2ebd4e275aff0b1d5370bb8a838a4
Muhammad Roman <mromankhan005@gmail.com>
2024-11-08 23:59:33 +0500

    JS bulb Task

---
+++ app.js
@@ -0,0 +1,51 @@
+let bulbEl = document.getElementsByClassName("off-light");
+// bulbEl[0].src = ""
+function light_on() {
+   bulbEl[0].src = "images/on-light.png";
+}
+// onBulb.innerHTML = className("onb");
+function light_off() {
+    bulbEl[0].src = "images/off-light.png"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
